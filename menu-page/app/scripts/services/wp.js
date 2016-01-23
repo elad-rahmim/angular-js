1aade1f136bf5d6337ced4971b113f203670db64
elad-rahamim <elad@eready.co.il>
2016-01-23 16:13:14 +0200

    created factory wp script

---
+++ menu-page/app/scripts/services/wp.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name menuPageApp.wp
+ * @description
+ * # wp
+ * Factory in the menuPageApp.
+ */
+angular.module('menuPageApp')
+  .factory('wp', function ($q, $http) {
+    // Service logic
+    // ...
+    var url = 'www.offerstake.com';
+    var wp = {};
+
+    wp.getCategory = function(){
+        return $http.get(url + '/api/get_category_index/');
+    };
+    wp.getPost = function(postId){
+        return $http.get(url+'/api/get_post/?post_id='+postId);
+    };
+
+    wp.getPosts = function(count,page,category){
+    var apiurl='';
+    if(category !== 'all'){
+        apiurl=url+'/api/get_category_posts/?slug='+category+'&count='+count+'&page='+page.toString();
+        }else{
+            apiurl=url+'/api/get_posts/?count='+count+'&page='+page.toString();
+            }
+
+        return $http.get(apiurl);
+    };
+        return wp;
+  });
