a46bafa82cad83f23fd691d55a6e30a4d98fe87a
elad-rahamim <elad@eready.co.il>
2016-01-23 15:51:55 +0200

    Create Angular Project
    Create Angular Project

---
+++ menu-page/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name menuPageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the menuPageApp
+ */
+angular.module('menuPageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ menu-page/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name menuPageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the menuPageApp
+ */
+angular.module('menuPageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
